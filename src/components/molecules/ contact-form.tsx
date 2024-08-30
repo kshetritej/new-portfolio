e413ebbb9bf30c9ba1bd2bbfdbdfe7287b759748
kshetritej <tejghartikshetri@gmail.com>
2024-08-30 16:29:45 +0530

    add: form submission and validation

--- src/components/molecules/ contact-form.tsx
+++ src/components/molecules/ contact-form.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-
+import axios from "axios";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -14,30 +14,83 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useMutation } from "@tanstack/react-query";
+import { useToast } from "@/components/ui/use-toast";
+import { Loader2 } from "lucide-react";
+
+const emailSchema = z
+  .string()
+  .email({
+    message: "Please enter a valid email address.",
+  })
+  .regex(/^[a-zA-Z0-9._%+-]+@(gmail\.com|outlook\.com)$/, {
+    message: "Sorry, but currently we only accept Gmail and Outlook.",
+  });
+const messageSchema = z.string().refine(
+  (value) => {
+    const wordCount = value.trim().split(/\s+/).length;
+    return wordCount >= 20;
+  },
+  {
+    message: "Your message must be at least 20 words long.",
+  }
+);
 
 const formSchema = z.object({
   fullName: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
-  email: z.string().email({
-    message: "Please enter a valid email address.",
-  }),
-  message: z.string().min(10, {
-    message: "Message must be at least 10 characters.",
-  }),
+  email: emailSchema,
+  message: messageSchema,
 });
 
-function onSubmit(data: any) {
-  console.log(data);
-}
+type EmailType = z.infer<typeof formSchema>;
 
 export default function ContactForm() {
+  const { toast } = useToast();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       fullName: "",
+      email: "",
+      message: "",
     },
   });
+
+  const { mutate: sendMail, isPending } = useMutation({
+    mutationKey: ["sendMail"],
+    mutationFn: async (data: EmailType) =>
+      await axios
+        .post(`${import.meta.env.VITE_API_URL}`, {
+          fullName: data.fullName,
+          email: data.email,
+          message: data.message,
+        })
+        .then((res) => {
+          return res.data;
+        }),
+    onSuccess: () => {
+      toast({
+        title: "Great news!",
+        description:
+          "Your message has been sent successfully. We'll get back to you soon. 😊",
+        variant: "success",
+      });
+      form.reset();
+    },
+    onError: () => {
+      toast({
+        title: "Oops! Something went wrong and we couldn't send your message.",
+        description:
+          " Please try again later, and sorry for the inconvenience. 😔",
+        variant: "destructive",
+      });
+    },
+  });
+
+  const onSubmit = (data: EmailType) => {
+    sendMail(data);
+  };
 
   return (
     <Form {...form}>
@@ -47,7 +100,10 @@ export default function ContactForm() {
       >
         <div className="form-title space-y-4">
           <h2 className="font-bold text-3xl">Chat with me directly.</h2>
-          <p>Want help with something? Want a demo? or<br /> Just want to know me?</p>
+          <p>
+            Want help with something? Want a demo? or
+            <br /> Just want to know me?
+          </p>
         </div>
         <FormField
           control={form.control}
@@ -56,7 +112,7 @@ export default function ContactForm() {
             <FormItem>
               <FormLabel>Full Name</FormLabel>
               <FormControl>
-                <Input placeholder="" {...field} />
+                <Input type="text" {...field} />
               </FormControl>
               <FormDescription>
                 This is your public display name.
@@ -95,7 +151,9 @@ export default function ContactForm() {
             </FormItem>
           )}
         />
-        <Button type="submit">Send</Button>
+        <Button type="submit">
+          {isPending ? <Loader2 className="animate-spin" /> : "Send"}
+        </Button>
       </form>
     </Form>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "../app/globals.css";
+import { Toaster } from "@/components/ui/toaster.tsx";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster />
+    </QueryClientProvider>
   </StrictMode>
 );
